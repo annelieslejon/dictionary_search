4207743a934bf2304f85c0d0ce05f4e0d725348a
Annelies Lejon <annelies.lejon@ugent.be>
2024-07-15 08:16:21 +0000

    new working version signbuddy create gloss db

--- dictionary_search/demo_webapp/create_glossdb.py
+++ dictionary_search/demo_webapp/create_glossdb.py
@@ -12,60 +12,14 @@ db_params = {
 }
 
 
+db_path = "/home/VGT/dictionary_search/db/embeddings_pf_160/"
 
-# Dutch translation of the ID Glosses that we record.
-glosses = [
-    "Bouwen",
-    "Waarom",
-    "Hebben",
-    "Paard",
-    "Melk",
-    "Herfst",
-    "Valentijn",
-    "Telefoneren",
-    "Straat",
-    "Haas",
-    "Hond",
-    "Rusten",
-    "School",
-    "Onthouden",
-    "Wat",
-    "Vliegtuig",
-    "Bel",
-    "Kat",
-    "Mama",
-    "Papa"
-]
-
-
-
+ids =  os.listdir(db_path)
+id_glosses = [id.split('.npy')[0] for id in ids]
 #ID Glosses that we record. Order should be the same as `glosses`.
-id_glosses = [
-    "BOUWEN-G-1906",
-    "WAAROM-A-13564",
-    "HEBBEN-A-4801",
-    "PAARD-A-8880",
-    "MELK-B-7418",
-    "HERFST-B-4897",
-    "VALENTIJN-A-16235",
-    "TELEFONEREN-D-11870",
-    "STRAAT-A-11560",
-    "HAAS-A-16146",
-    "HOND-A-5052",
-    "RUSTEN-B-10250",
-    "SCHOOL-A-10547",
-    "ONTHOUDEN-A-8420",
-    "WAT-A-13657",
-    "VLIEGTUIG-B-13187",
-    "KLEPELBEL-A-1166",
-    "POES-G-9372",
-    "MOEDER-A-7676",
-    "VADER-G-8975"
-]
 
 
-
-data_to_insert = zip(glosses, id_glosses)
+data_to_insert = id_glosses
 
 def insert_multiple_entries(db_params, data_to_insert):
     try:
@@ -73,13 +27,13 @@ def insert_multiple_entries(db_params, data_to_insert):
         conn = psycopg2.connect(**db_params)
         conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
         cursor = conn.cursor()
+        insert_query = "INSERT INTO glosses (gloss_full) VALUES (%s)"
 
-        # SQL to insert data
-        query = sql.SQL("INSERT INTO glosses (gloss_name, gloss_full) VALUES (%s, %s);")
+        data_tuples = [(name,) for name in data_to_insert]
 
         # Execute the query with multiple data
-        cursor.executemany(query, data_to_insert)
-
+        cursor.executemany(insert_query, data_tuples)
+        conn.commit()
         print("Data inserted successfully")
 
     except (Exception, psycopg2.DatabaseError) as error:
