02ce17d08cac73b342ea98a69b9b347da6a4873c
Annelies Lejon <annelies.lejon@ugent.be>
2024-07-15 08:08:22 +0000

    new working version signbuddy search engine

--- dictionary_search/demo_webapp/search_engine.py
+++ dictionary_search/demo_webapp/search_engine.py
@@ -14,40 +14,51 @@ class DatabaseEntry:
     gloss: str
 
 
+
+
+def cosine_similarity(emb_1, emb_2):
+
+    return np.dot(emb_2, emb_1) / (np.linalg.norm(emb_1) * np.linalg.norm(emb_2))
+
 class SearchEngine:
     """Allows searching the database."""
 
-    def __init__(self, database_path: str, max_entries: int):
+    def __init__(self, database_path: str, max_entries: int,query_set):
         self.database = []
+        self.rng = np.random.default_rng(10)
 
         # We first add the signs that are in the query set and then continue to add unique signs until we reach max_entries.
         # We do this in a deterministic manner of course...
-        db_entries = [os.path.join(database_path, 'HEBBEN-A-4801.npy'),  # In corpus.
-                      os.path.join(database_path, 'TELEFONEREN-D-11870.npy'),  # In corpus.
-                      os.path.join(database_path, 'HAAS-A-16146.npy'),  # In corpus.
-                      os.path.join(database_path, 'STRAAT-A-11560.npy'),  # In corpus.
-                      os.path.join(database_path, 'PAARD-A-8880.npy'),  # In corpus.
-                      os.path.join(database_path, 'HOND-A-5052.npy'),  # In corpus.
-                      os.path.join(database_path, 'RUSTEN-B-10250.npy'),  # In corpus.
-                      os.path.join(database_path, 'SCHOOL-A-10547.npy'),  # In corpus.
-                      os.path.join(database_path, 'ONTHOUDEN-A-8420.npy'),  # In corpus.
-                      os.path.join(database_path, 'WAT-A-13657.npy'),  # In corpus.
+        if (query_set is not None):
+            db_entries = query_set
 
-                      os.path.join(database_path, 'BOUWEN-G-1906.npy'),  # Not in corpus.
-                      os.path.join(database_path, 'WAAROM-A-13564.npy'),  # Not in corpus.
-                      os.path.join(database_path, 'MELK-B-7418.npy'),  # Not in corpus.
-                      os.path.join(database_path, 'VALENTIJN-A-16235.npy'),  # Not in corpus.
-                      os.path.join(database_path, 'HERFST-B-4897.npy'), # Not in corpus.
-                      os.path.join(database_path, 'VLIEGTUIG-B-13187.npy'),  # Not in corpus.
-                      os.path.join(database_path, 'KLEPELBEL-A-1166.npy'),  # Not in corpus.
-                      os.path.join(database_path, 'POES-G-9372.npy'),  # Not in corpus.
-                      os.path.join(database_path, 'MOEDER-A-7676.npy'),  # Not in corpus.
-                      os.path.join(database_path, 'VADER-G-8975.npy')]  # Not in corpus.
+        else:
+            db_entries = [os.path.join(database_path, 'HEBBEN-A-4801.npy'),  # In corpus.
+                          os.path.join(database_path, 'TELEFONEREN-D-11870.npy'),  # In corpus.
+                          os.path.join(database_path, 'HAAS-A-16146.npy'),  # In corpus.
+                          os.path.join(database_path, 'STRAAT-A-11560.npy'),  # In corpus.
+                          os.path.join(database_path, 'PAARD-A-8880.npy'),  # In corpus.
+                          os.path.join(database_path, 'HOND-A-5052.npy'),  # In corpus.
+                          os.path.join(database_path, 'RUSTEN-B-10250.npy'),  # In corpus.
+                          os.path.join(database_path, 'SCHOOL-A-10547.npy'),  # In corpus.
+                          os.path.join(database_path, 'ONTHOUDEN-A-8420.npy'),  # In corpus.
+                          os.path.join(database_path, 'WAT-A-13657.npy'),  # In corpus.
+
+                          os.path.join(database_path, 'BOUWEN-G-1906.npy'),  # Not in corpus.
+                          os.path.join(database_path, 'WAAROM-A-13564.npy'),  # Not in corpus.
+                          os.path.join(database_path, 'MELK-B-7418.npy'),  # Not in corpus.
+                          os.path.join(database_path, 'VALENTIJN-A-16235.npy'),  # Not in corpus.
+                          os.path.join(database_path, 'HERFST-B-4897.npy'), # Not in corpus.
+                          os.path.join(database_path, 'VLIEGTUIG-B-13187.npy'),  # Not in corpus.
+                          os.path.join(database_path, 'KLEPELBEL-A-1166.npy'),  # Not in corpus.
+                          os.path.join(database_path, 'POES-G-9372.npy'),  # Not in corpus.
+                          os.path.join(database_path, 'MOEDER-A-7676.npy'),  # Not in corpus.
+                          os.path.join(database_path, 'VADER-G-8975.npy')]  # Not in corpus.
 
         all_db_entries: List[str] = glob.glob(os.path.join(database_path, '*.npy'))
         indices = list(range(len(all_db_entries)))
-        random.seed(10)
-        random.shuffle(indices)
+
+        self.rng.shuffle(indices)
         with open('/tmp/indices.txt', 'w') as f:
             f.writelines([str(i) + '\n' for i in indices])
         all_db_entries = list(np.array(all_db_entries)[indices])
@@ -69,18 +80,14 @@ class SearchEngine:
         :param embedding: The query embedding.
         :returns: The search results, a list of glosses and their distance to the query."""
         distances = []
-        scaled_distances = []
-        distances_random = []
         for i, key in enumerate(self.database):
-            eucdist = np.linalg.norm(embedding - key.embedding)
-
-
-
-            distances.append((eucdist, i))
+            cossim = cosine_similarity(embedding, key.embedding)
+            dist=  1-cossim # np.linalg.norm(embedding - key.embedding) #
+            score = (1+cossim)/2
+            distances.append((dist, i,score))
         ordered = sorted(distances, key=lambda tup: tup[0])
-        scores = np.random.rand(len(ordered))
 
-        results = [(self.database[i].gloss, i, float(d)) for s,(d, i) in zip(scores,ordered)]
+        results = [(self.database[i].gloss, i, float(s),float(d)) for (d, i,s) in ordered]
 
 
         return results
